110f62f7a15243e0f8595cfba142032a93f3c57a
slayer0x10 <saicharanruka@gmail.com>
2026-02-14 22:48:39 +0530

    Started Hands on ML Book
    Test comment description

---
+++ hands-on-ml/helpers.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+from zlib import crc32
+
+
+def split_train_test(data: pd.DataFrame, test_ratio: float):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+def split_train_test_by_id(data: pd.DataFrame, test_ratio: float, id_column: int):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+def test_set_check(identifier, test_ratio: float):
+    return crc32(np.int64(identifier)) & 0xFFFFFFFF < test_ratio * 2**32
